5aa8782cf445463116129fc0223e7833c31fa644
Edison <edison16029@gmail.com>
2021-03-10 09:02:52 +0530

    Generic Tab View

--- src/components/MatchedBooksTab.js
+++ src/components/MatchedBooksTab.js
@@ -1,82 +1,42 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
 import { List, Modal, Button, Typography } from "antd";
+import { Pagination } from 'antd';
+import TextItem from './presentationalComponents/TextItem';
+import TabView from './presentationalComponents/TabView';
+
+import '../styles/styles.scss';
 import "../styles/matchedbooks.scss";
 const { Title, Text } = Typography;
-const data = [
-  {
-    title: "Ant Design Title 1",
-  },
-  {
-    title: "Ant Design Title 2",
-  },
-  {
-    title: "Ant Design Title 3",
-  },
-  {
-    title: "Ant Design Title 4",
-  },
-];
+
 
 const mapStateToProps = (state) => ({
   myAccount: state.myAccount,
 });
 
 const MatchedBooksTab = ({ myAccount }) => {
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const showModal = () => {
-    setIsModalVisible(true);
-  };
-  const handleOk = () => {
-    setIsModalVisible(false);
-  };
+  // const [isModalVisible, setIsModalVisible] = useState(false);
+  // const showModal = () => {
+  //   setIsModalVisible(true);
+  // };
+  // const handleOk = () => {
+  //   setIsModalVisible(false);
+  // };
 
-  const handleCancel = () => {
-    setIsModalVisible(false);
-  };
+  // const handleCancel = () => {
+  //   setIsModalVisible(false);
+  // };
+
+  const data = [];
+  for(var i = 0; i< 48; i++){
+    data.push({title : "Hello " + i})
+  }
+
   return (
-    <div className="accounttab-container">
-      <Title level={2} className="align-center">Books you are interested in</Title>
-      <List
-        size="large"
-        itemLayout="horizontal"
-        dataSource={data}
-        renderItem={(item) => (
-          <List.Item>
-            <Button type="link" block onClick={showModal}>
-              {item.title}
-            </Button>
-            <Modal
-              title="Book Details"
-              visible={isModalVisible}
-              onOk={handleOk}
-              onCancel={handleCancel}
-              centered
-              footer={[
-                <Button key="submit" type="primary" onClick={handleOk}>
-                  Remove book from interested books
-                </Button>,
-              ]}
-            >
-              <div className="modal-container">
-              <div className="align-center">
-                <Title level={5}>Name</Title>
-                  <Text keyboard>Ant Design (keyboard)</Text>
-                </div>
-                <div className="align-center">
-                <Title level={5}>Author</Title>
-                  <Text keyboard>Ant Design (keyboard)</Text>
-                </div>
-                <div className="align-center">
-                <Title level={5}>Owner</Title>
-                  <Text keyboard>Ant Design (keyboard)</Text>
-                </div>
-              </div>
-            </Modal>
-          </List.Item>
-        )}
-      />
-    </div>
+    <TabView 
+      data = {data}
+      title = {"Books you are interested in"}
+      numberOfBooksPerPage = {5} />
   );
 };
 
