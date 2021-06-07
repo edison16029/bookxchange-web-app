445ba56afac80f50f96f41640fc654f9b01dc895
pranavram98 <51071662+pranavram98@users.noreply.github.com>
2021-06-07 21:07:00 +0530

    notification-onclick (#23)
    * add modal intial
    
    * notification on click

--- src/components/presentationalComponents/Layout.js
+++ src/components/presentationalComponents/Layout.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import { Menu, Dropdown, Button } from "antd";
 import { BellFilled } from "@ant-design/icons";
 import "../../styles/styles.scss";
@@ -14,7 +15,7 @@ const mapStateToProps = (state) => ({
   notifications: state.profile.data.accountInfo.notifications,
   timestamp: state.profile.data.accountInfo.timestamp,
 });
-const readNotifs = (timestamp,updateNotifs) => {
+const readNotifs = (timestamp, updateNotifs) => {
   const requestBody = {
     timestamp: timestamp,
   };
@@ -33,20 +34,39 @@ const readNotifs = (timestamp,updateNotifs) => {
       handleApiError(error);
     });
 };
+
 const Layout = (props) => {
-  const { notifications, timestamp,updateNotifs } = props;
+  const { notifications, timestamp, updateNotifs } = props;
   let newNotifs = notifications === undefined ? [] : notifications;
+  let history = useHistory();
+  const onNotificationClick = ({ item, key }) => {
+    var email = item.props.children[1].match(/\((.*)\)/);
+    if (email !== null) {
+      window.location.href = `mailto:${email[1]}?subject=Exchanging%20Books`;
+    } else {
+      history.push("/matchedbooks");
+    }
+  };
+
   const menu = (
-    <Menu>
+    <Menu onClick={onNotificationClick}>
       <Menu.ItemGroup title="Notifications">
         {newNotifs.length !== 0 ? (
-          notifications.map((value, index) => <Menu.Item key={index}>{value.text}</Menu.Item>)
+          notifications.map((value) => (
+            <Menu.Item key={value.userId}>{value.text}</Menu.Item>
+          ))
         ) : (
           <Menu.Item>{"No New Notifications"}</Menu.Item>
         )}
       </Menu.ItemGroup>
       {newNotifs.length !== 0 ? (
-        <Button type="secondary" style={{backgroundColor : '#115173', color: 'white'}} onClick={() => readNotifs(timestamp,updateNotifs)}>Mark as Read</Button>
+        <Button
+          type="secondary"
+          style={{ backgroundColor: "#115173", color: "white" }}
+          onClick={() => readNotifs(timestamp, updateNotifs)}
+        >
+          Mark as Read
+        </Button>
       ) : null}
     </Menu>
   );
@@ -67,4 +87,4 @@ const Layout = (props) => {
     </div>
   );
 };
-export default connect(mapStateToProps, { updateNotifs})(Layout);
+export default connect(mapStateToProps, { updateNotifs })(Layout);
